c2c71eebf146fc596bc60bf535ef4e9273c94719
djstava <djstava@gmail.com>
2015-05-06 17:05:45 +0800

    colorBlobDetector

--- app/src/main/java/com/macernow/djstava/handdetection/MainActivity.java
+++ app/src/main/java/com/macernow/djstava/handdetection/MainActivity.java
@@ -1,5 +1,7 @@
 package com.macernow.djstava.handdetection;
 
+import android.graphics.Camera;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,17 +11,52 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfInt4;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener,View.OnTouchListener {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    private Mat mRgba,mGray;
+    private Mat mSpectrum;
+    private Scalar mBlobColorRgba;
+    private Scalar mBlobColorHsv;
+    private ColorBlobDetector mDetector;
+    private Size SPECTRUM_SIZE;
+    private Scalar CONTOUR_COLOR;
+    private Scalar CONTOUR_COLOR_WHITE;
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean mIsColorSelected = false;
+
+    final Handler mHandler = new Handler();
+    int numberOfFingers = 0;
+
+    final Runnable mUpdateFingerCountResults = new Runnable() {
+        public void run() {
+            updateNumberOfFingers();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +65,8 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
-        cameraBridgeViewBase = (CameraBridgeViewBase)findViewById(R.id.HelloOpenCvView);
-        cameraBridgeViewBase.setVisibility(View.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.javaCameraView);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
@@ -39,7 +75,8 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                     Log.e(TAG,"OpenCV loaded successfully.");
-                    cameraBridgeViewBase.enableView();
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
                     break;
 
                 default:
@@ -52,8 +89,8 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
     @Override
     public void onPause() {
         super.onPause();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
         }
     }
 
@@ -66,33 +103,225 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
         }
     }
 
     public boolean onTouch(View view,MotionEvent motionEvent) {
-        return false;
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
+        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
+
+        int x = (int)motionEvent.getX() - xOffset;
+        int y = (int)motionEvent.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE);
+
+        mIsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
+
+    //from h3ct0r old SDK
+    //public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+    public Mat onCameraFrame(Mat mat) {
+        Log.d(TAG,"==== djstava onCameraFrame mat===");
+
+        //mRgba = inputFrame.rgba();
+        //mGray = inputFrame.gray();
+
+        mRgba = mat;
+
+        Imgproc.GaussianBlur(mRgba,mRgba,new Size(3,3),1,1);
+
+        if (!mIsColorSelected) {
+            return mRgba;
+        }
+
+        List<MatOfPoint> contours = mDetector.getContours();
+        mDetector.process(mRgba);
+
+        if (contours.size() <= 0) {
+            return mRgba;
+        }
+
+        RotatedRect rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(0).toArray()));
+
+        double boundWidth = rect.size.width;
+        double boundHeight = rect.size.height;
+        int boundPos = 0;
+
+        for (int i = 1; i < contours.size();i++) {
+            rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+            if (rect.size.width * rect.size.height > boundWidth * boundHeight) {
+                boundWidth = rect.size.width;
+                boundHeight = rect.size.height;
+                boundPos = i;
+            }
+        }
+
+        Rect boundRect = Imgproc.boundingRect(new MatOfPoint(contours.get(boundPos).toArray()));
+        Core.rectangle(mRgba,boundRect.tl(),boundRect.br(),CONTOUR_COLOR_WHITE,2,8,0);
+
+        int rectHeightThresh = 0;
+        double a = boundRect.br().y - boundRect.tl().y;
+        a = a * 0.7;
+        a = boundRect.tl().y + a;
+
+        Log.d(TAG,
+                " A ["+a+"] br y - tl y = ["+(boundRect.br().y - boundRect.tl().y)+"]");
+
+        //Core.rectangle( mRgba, boundRect.tl(), boundRect.br(), CONTOUR_COLOR, 2, 8, 0 );
+        Core.rectangle( mRgba, boundRect.tl(), new Point(boundRect.br().x, a), CONTOUR_COLOR, 2, 8, 0 );
+
+        MatOfPoint2f pointMat = new MatOfPoint2f();
+        Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(boundPos).toArray()), pointMat, 3, true);
+        contours.set(boundPos, new MatOfPoint(pointMat.toArray()));
+
+        MatOfInt hull = new MatOfInt();
+        MatOfInt4 convexDefect = new MatOfInt4();
+        Imgproc.convexHull(new MatOfPoint(contours.get(boundPos).toArray()), hull);
+
+        if(hull.toArray().length < 3) return mRgba;
+
+        Imgproc.convexityDefects(new MatOfPoint(contours.get(boundPos)	.toArray()), hull, convexDefect);
+
+        List<MatOfPoint> hullPoints = new LinkedList<MatOfPoint>();
+        List<Point> listPo = new LinkedList<Point>();
+        for (int j = 0; j < hull.toList().size(); j++) {
+            listPo.add(contours.get(boundPos).toList().get(hull.toList().get(j)));
+        }
+
+        MatOfPoint e = new MatOfPoint();
+        e.fromList(listPo);
+        hullPoints.add(e);
+
+        List<MatOfPoint> defectPoints = new LinkedList<MatOfPoint>();
+        List<Point> listPoDefect = new LinkedList<Point>();
+        for (int j = 0; j < convexDefect.toList().size(); j = j+4) {
+            Point farPoint = contours.get(boundPos).toList().get(convexDefect.toList().get(j+2));
+            Integer depth = convexDefect.toList().get(j+3);
+            /*
+            if(depth > iThreshold && farPoint.y < a){
+                listPoDefect.add(contours.get(boundPos).toList().get(convexDefect.toList().get(j+2)));
+            }
+            */
+            Log.d(TAG, "defects ["+j+"] " + convexDefect.toList().get(j+3));
+        }
+
+        MatOfPoint e2 = new MatOfPoint();
+        e2.fromList(listPo);
+        defectPoints.add(e2);
+
+        Log.d(TAG, "hull: " + hull.toList());
+        Log.d(TAG, "defects: " + convexDefect.toList());
+
+        Imgproc.drawContours(mRgba, hullPoints, -1, CONTOUR_COLOR, 3);
+
+        int defectsTotal = (int) convexDefect.total();
+        Log.d(TAG, "Defect total " + defectsTotal);
+
+        this.numberOfFingers = listPoDefect.size();
+        if(this.numberOfFingers > 5) this.numberOfFingers = 5;
+
+        mHandler.post(mUpdateFingerCountResults);
+
+        for(Point p : listPoDefect){
+            Core.circle(mRgba, p, 6, new Scalar(255,0,255));
+        }
+
+        return mRgba;
     }
 
     /*
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
-        return cvCameraViewFrame.rgba();
+    public Mat onCameraFrame(Mat mat) {
+        mRgba = mat;
+
+        Log.d(TAG,"==== djstava onCameraFrame mat===");
+        if (mIsColorSelected) {
+            mDetector.process(mRgba);
+            List<MatOfPoint> contours = mDetector.getContours();
+            Log.e(TAG, "Contours count: " + contours.size());
+            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(mBlobColorRgba);
+
+            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+            mSpectrum.copyTo(spectrumLabel);
+        }
+
+        return mRgba;
     }
     */
 
-    public Mat onCameraFrame(Mat inputFrame) {
-        Mat mat = inputFrame;
-
-        return mat;
-    }
-
     public void onCameraViewStarted(int width,int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
 
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mDetector = new ColorBlobDetector();
+        mSpectrum = new Mat();
+        mBlobColorRgba = new Scalar(255);
+        mBlobColorHsv = new Scalar(255);
+        SPECTRUM_SIZE = new Size(200, 64);
+        CONTOUR_COLOR = new Scalar(255,0,0,255);
+        CONTOUR_COLOR_WHITE = new Scalar(255,255,255,255);
     }
 
     public void onCameraViewStopped() {
+        mRgba.release();
+        mGray.release();
+    }
 
+    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
+    }
+
+    public void updateNumberOfFingers(){
+        String str = String.valueOf(numberOfFingers);
+        Toast.makeText(MainActivity.this,str,Toast.LENGTH_LONG).show();
+        //numberOfFingersText.setText(String.valueOf(this.numberOfFingers));
     }
 
     @Override
